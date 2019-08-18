c04873868c06a68f5fa37acef2b33df8af296f72
Shokichi <hoangvanlamcntt@gmail.com>
2019-08-18 12:54:31 +0700

    Update load code

---
+++ petshop/footer.php
@@ -0,0 +1,43 @@
+<!--Đây là phần cuối-->
+<div style="clear: both;"></div>
+<div>
+      <hr />
+        <div class="header-line-Footer"> <img src="images/icon/logo.png" alt="logo" name="logo" />
+          <ul>
+            <li class="menuFooter">&emsp;<a href="index.php" class="format-url">Trang Chủ</a></li>
+            <li class="menuFooter">&emsp;<a href="gioithieu.php" class="format-url">Giới Thiệu</a></li>
+            <li class="menuFooter">&emsp;<a href="sanpham.php" class="format-url">Sản phẩm</a></li>
+            <li class="menuFooter">&emsp;<a href="#" class="format-url">Tin Tức</a></li>
+            <li class="menuFooter">&emsp;<a href="tel:0989954169" class="format-url">Liên Hệ</a></li>
+          </ul>
+        </div>
+        <div class="footer-container">
+          <ul class="footer-table-1">
+            <h3> CHÚNG TÔI Ở ĐÂY</h3>
+            <li><img src="images/icon/vitri.png" width="20" height="20"/>&emsp;70 Tô Ký, phường Tân Chánh Hiệp, Q.12, Tp. Hồ Chí Minh</li>
+            <li><img src="images/icon/icon-dienthoai.png" width="20" height="20"/>&emsp;Hotline :19008198</li>
+            <li><img src="images/icon/icon-phone-new.gif" width="20" height="20"/>&emsp; Điện thoại : 098.995.4169</li>
+            <li><img src="images/icon/mail.png" width="20" height="20"/>&emsp;info@petshop.com</li>
+          </ul>
+          <ul class="footer-table-1">
+            <h3> VỀ CHÚNG TÔI </h3>
+            <li><a href="index.php" class="format-url"> Trang Chủ</a></li>
+            <li><a href="gioithieu.php" class="format-url"> Giới Thiệu</a></li>
+            <li><a href="sanpham.php" class="format-url">Sản Phẩm</a></li>
+            <li><a href="#" class="format-url"> Tin Tức</a></li>
+          </ul>
+          <ul class="footer-table-1" >
+            <h3> HƯỚNG DẪN MUA HÀNG </h3>
+            <li><a href="#" class="format-url"> Hướng dẫn mua hàng</a></li>
+              <li><a href="#" class="format-url"> Hướng dẫn chăm sóc thú nuôi</a></li>
+              <li><a href="#" class="format-url"> Hướng dẫn tra cứu thông tin</a></li>
+          </ul>
+          <ul class="footer-table-1">
+            <h3> CHÍNH SÁCH </h3>
+            <li><a href="#" class="format-url"> Chính sách thanh toán</a></li>
+              <li><a href="#" class="format-url"> Chính sách ưu đãi</a></li>
+          </ul>
+        </div>
+      </div>
+       <div style="clear:both"></div>
+       <div align="center">&copy;2018 - Thiết kế bởi nhóm PetShop</div>
